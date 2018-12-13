097b4f8135a327c1f398086cb71d814859becb59
Minenash <minenash@protonmail.com>
2018-12-12 20:56:08 -0500

    Updated command structure

--- src/main/java/com/redstoner/modules/motd/Motd.cmd
+++ src/main/java/com/redstoner/modules/motd/Motd.cmd
@@ -1,11 +1,14 @@
-command setmotd {
-    [string:motd...] {
+command motd {
+    get {
+        help Returns the motd;
+        run getmotd;
+    	perm utils.motd.get;
+    }
+    set [string:motd...] {
         help Sets the motd. Use --reset to reset to default;
         run setmotd motd;
     	perm utils.motd.set;
     }
-}
-command getmotd {
     [empty] {
         help Returns the motd;
         run getmotd;
